ae8481c520b9f62d1cb087fd0ee5797822970a53
YajirobeFromDC <matteoc@ya.ru>
2021-01-27 16:12:39 +0300

    feat: add slider, element after slider and products of the week section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function(){
-  
-})
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false
+  });
+  let mixer = mixitup('.products-of-the-week__list');
+});
+
+
+
+
+function handleFirstTab(e) {
+  if (e.keyCode === 9) { // tab key
+      document.body.classList.add('user-is-tabbing');
+      window.removeEventListener('keydown', handleFirstTab);
+  }
+}
+window.addEventListener('keydown', handleFirstTab);
